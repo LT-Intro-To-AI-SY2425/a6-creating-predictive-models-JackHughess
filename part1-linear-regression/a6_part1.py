54cde35a742ff70e140e7217b2fa76663fc0d3d2
Jack Hughes <jack.hughes1452@gmail.com>
2024-12-05 11:03:29 -0600

    part 1 done

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -3,22 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
-x = data["Age"].values
-y = data["Blood Pressure"].values
+df = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
+x = df["Age"].values
+y = df["Blood Pressure"].values
 
 # Use reshape to turn the x values into 2D arrays:
 x = x.reshape(-1,1)
 
 # Create the model
-
+model = LinearRegression().fit(x, y)
 # Find the coefficient, bias, and r squared values. 
-# Each should be a float and rounded to two decimal places. 
+# Each should be a float and rounded to two decimal places.wh
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
+
+x_predict = 43
+prediction = model.predict([[x_predict]])
 
 
 # Print out the linear equation and r squared value
-
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+print(f"Prediction when x is {x_predict}: {prediction}")
 # Predict the the blood pressure of someone who is 43 years old.
 # Print out the prediction
 
-# Create the model in matplotlib and include the line of best fit
+
+
+plt.figure(figsize=(6,4))
+
+
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
+plt.xlabel("Temperature °F")
+plt.ylabel("Chirps per Minute")
+plt.title("Cricket Chirps by Temperature")
+
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show()
--- part1-linear-regression/part1_writeup.md
+++ part1-linear-regression/part1_writeup.md
@@ -6,6 +6,12 @@ After completing `a6_part1.py` answer the following questions
 
 1. What is the r squared value?  What does this say about this linear regression model?
 
+The r squared value shows how well that the line fits the data. It shows how far, on average, the distance to each point from the line is squared on a scale from 0 to 1. This says that the value is .626 meaning that the line doesn't fit the data very well which could be because of other factors that we are not taking into account.
+
 2. According to your model, what is the predicted systolic blood pressure for a person who is 42 years old?
 
+137.46185286
+
 3. How accurate do you think this model's predictions are?  Do you think this model is accurate enough to reliably predict someone's blood pressure based on their age?  Why or why not?  And if not, what could improve the model?
+
+I think it is more like an estimate but I would not use it to predict blood pressure reliably because the r^2 is only .62. Most scientific data sets require a r^2 of .95 or higher so I wouldn't use this. To improve the model we could give it more data and also add more features to explain all of the varience.
